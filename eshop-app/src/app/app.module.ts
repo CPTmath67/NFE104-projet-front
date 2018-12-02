7ec7cb88d3d0d344c019bc2cee88781b04c4c3de
mathieu.k <kernmathieu@gmail.com>
2018-12-02 21:03:47 +0100

    ajout navbar + composants listeproduits

--- eshop-app/src/app/app.module.ts
+++ eshop-app/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ListeProduitsComponent } from './liste-produits/liste-produits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListeProduitsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
