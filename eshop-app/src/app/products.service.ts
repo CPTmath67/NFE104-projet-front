052d0f14e4240be0ccd5e57eed04fcf9f4e7f032
Mathieu KERN <kernmathieu@gmail.com>
2019-01-09 13:14:12 +0100

    configure observable get all method

--- eshop-app/src/app/products.service.ts
+++ eshop-app/src/app/products.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private baseUrl = `http://fakerestapi.azurewebsites.net/api`;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public getAll(): Observable<any>{
+    return this.http.get(`${this.baseUrl}/Books`).
+      pipe(
+        map((data) =>{
+          return data;
+        }, (err) =>{
+          console.log('error', err)
+        })
+      );
+  }
+  // public getMovieById(id:number): Observable<any>{
+  //   return this.http.get(`${this.baseUrl}/Books/${id}`).
+  //     pipe(
+  //       map((data)=>{
+  //         return data.json();
+  //       },(err) =>{
+  //         console.log('error',err)
+  //       }
+
+  //       ));
+  // }
 }
