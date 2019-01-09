de8f48f6043a987bf798b7da51c2b7f3dca30622
Mathieu KERN <kernmathieu@gmail.com>
2019-01-09 13:15:16 +0100

    products component: test display with fake api

--- eshop-app/src/app/products/products.component.ts
+++ eshop-app/src/app/products/products.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
+
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  displayedColumns: string[] = ['Title'];
+  dataSource;
+  constructor( private productsService: ProductsService) { 
+    this.productsService.getAll().subscribe( data =>{
+      console.log(data);
+      this.dataSource = data;
+    });
   }
 
+  ngOnInit() {
+
+  }
 }
