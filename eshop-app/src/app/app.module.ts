3c9ce88ca996c2891b18bdf574b26d331193c833
mkpanic <kernmathieu@gmail.com>
2019-01-20 22:39:44 +0100

    initiate product-detail compoenent

--- eshop-app/src/app/app.module.ts
+++ eshop-app/src/app/app.module.ts
@@ -12,15 +12,23 @@ import {
 import { PanierComponent } from './panier/panier.component';
 import { HomeComponent } from './home/home.component';
 import { ConnexionComponent } from './connexion/connexion.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccountCreationComponent } from './account-creation/account-creation.component';
 import { ProductsComponent } from './products/products.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ItemComponent } from './item/item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
   const routes : Routes = [
       { path: '', component: HomeComponent },
       { path: 'connexion', component: ConnexionComponent },
-      { path: 'account-creation', component: AccountCreationComponent }
+      { path: 'account-creation', component: AccountCreationComponent },
+      { path: 'products-list', component: ProductsComponent },
+      { path: 'product-detail/:id', component: ProductDetailComponent },
+      { path: 'product-add', component: ProductAddComponent },
+      { path: 'product-edit', component: ProductEditComponent }
   ]
 
 @NgModule({
@@ -29,7 +37,7 @@ import { HttpClientModule } from '@angular/common/http';
     NavbarComponent,
     PanierComponent,
     HomeComponent, 
-    ConnexionComponent, AccountCreationComponent, ProductsComponent
+    ConnexionComponent, AccountCreationComponent, ProductsComponent, ItemComponent, ProductDetailComponent, ProductAddComponent, ProductEditComponent
   ],
 
   imports: [
@@ -48,6 +56,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatToolbarModule,
     MatTabsModule,
     FormsModule,
+    ReactiveFormsModule,
    MatRadioModule,
    RouterModule.forRoot(routes),
   ],
