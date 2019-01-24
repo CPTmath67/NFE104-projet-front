68cf900c28a203db1f91a27ae82fb4d355e591fb
mkpanic <kernmathieu@gmail.com>
2019-01-24 12:05:06 +0100

    panier3

---
+++ eshop-app/src/app/basket.ts
@@ -0,0 +1,4 @@
+export class Basket {
+    basketList: any []; 
+    quantity: any[]; 
+}
--- eshop-app/src/app/panier/panier.component.ts
+++ eshop-app/src/app/panier/panier.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product';
+import { Basket } from '../product';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-panier',
   templateUrl: './panier.component.html',
-  styleUrls: ['./panier.component.scss']
+  styleUrls: ['./panier.component.scss']    
 })
 export class PanierComponent implements OnInit {
 
-  constructor() { }
 
+  dataSource;
+  product: Product = { idArticle: null, label: '', description: '', priceHT: null, priceTTC: null, delievryTime: null, picture: null, activate: null};
+
+  constructor(private route: ActivatedRoute, private productsService: ProductsService, private router: Router) { 
+    this.route.params.subscribe(param =>{
+      this.productsService.getProductById(param.idArticle).subscribe(data =>{
+        this.dataSource = data;
+        console.log(data);
+      })
+    }); 
+  }  
+  
   ngOnInit() {
   }
 
